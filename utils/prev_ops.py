bd03ced5861c3e6219bf863ca834faa5b6d39627
Mikhail Gribkov <mishacool4@gmail.com>
2023-07-05 21:02:50 +0300

    added data filter for operations

--- utils/prev_ops.py
+++ utils/prev_ops.py
@@ -4,14 +4,20 @@ import reformat
 
 def prev_ops():
     """Функция вызывает 5 последних операций из файла данных"""
-    data = json.load(open('operations.json', encoding='utf8'))
+    data_init = json.load(open('operations.json', encoding='utf8'))
+    data = []
+    for item in data_init:
+        if item.get("state") == "EXECUTED":
+            data.append(item)
     i = 0
     output = ''
     while i < 5:
-        if data[i]['state'] == 'EXECUTED':
-            output += f"{reformat.date_reformat(data[i]['date'])} {data[i]['description']}\n"
-            output += f"{reformat.transaction_details(data[i].get('from', ''))} -> " \
-                      f"{reformat.transaction_details(data[i].get('to', ''))}"
-            output += f"\n{data[i]['operationAmount']['amount']} {data[i]['operationAmount']['currency']['name']}\n\n"
-            i += 1
+        output += f"{reformat.date_reformat(data[i]['date'])} {data[i]['description']}\n"
+        output += f"{reformat.transaction_details(data[i].get('from', ''))} -> " \
+                  f"{reformat.transaction_details(data[i].get('to', ''))}"
+        output += f"\n{data[i]['operationAmount']['amount']} {data[i]['operationAmount']['currency']['name']}\n\n"
+        i += 1
     return output
+
+
+print(prev_ops())
